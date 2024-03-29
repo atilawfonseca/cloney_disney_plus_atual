64533ae6acc03811a1a2506c5820a28e8fa96b14
Atila Fonseca <eng.atila.sete@gmail.com>
2024-03-29 13:34:53 -0300

    ultimos ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const img = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function script() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -14,8 +21,9 @@ function compressImg(){
     .pipe(gulp.dest('./dist/img'))
 }
 
-exports.default = gulp.parallel(styles, compressImg);
+exports.default = gulp.parallel(styles, compressImg, script);
 
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', {ignoreInitial:false}, gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', {ignoreInitial:false}, gulp.parallel(script))
 }
